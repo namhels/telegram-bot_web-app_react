01ae9395568ff81ce4b9601b183932a6ede9059a
Serhii Lekhman <8.lsv.888@gmail.com>
2023-12-21 20:02:29 +0100

    update products

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -11,7 +11,7 @@ const products = [
     {id: '4', title: 'Jacket 8', price: 122, description: 'Green color, warm'},
     {id: '5', title: 'Jeans 3', price: 5000, description: 'Blue, straight'},
     {id: '6', title: 'Jacket 7', price: 600, description: 'Green color, warm'},
-    {id: '7', title: 'Jeans 4', price: 5500, description: 'Blue, straight'},
+    {id: '7', title: 'Trousers', price: 5500, description: 'Brown, stretch'},
     {id: '8', title: 'Jacket 5', price: 12000, description: 'Green color, warm'},
 ]
 
