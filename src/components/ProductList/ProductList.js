c0e0539657d35d902051fc19a1196117aa155f13
Serhii Lekhman <8.lsv.888@gmail.com>
2023-12-22 20:06:49 +0100

    update

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -7,7 +7,7 @@ import {useCallback, useEffect} from "react";
 const products = [
     {id: '1', title: 'Jeans', price: 5000, description: 'Blue, straight'},
     {id: '2', title: 'Jacket', price: 12000, description: 'Green color, warm'},
-    {id: '3', title: 'Jeans 2', price: 5000, description: 'Blue, straight'},
+    {id: '3', title: 'Underpants', price: 500, description: 'Black, stretch '},
     {id: '4', title: 'Jacket 8', price: 122, description: 'Green color, warm'},
     {id: '5', title: 'Jeans 3', price: 5000, description: 'Blue, straight'},
     {id: '6', title: 'Jacket 7', price: 600, description: 'Green color, warm'},
