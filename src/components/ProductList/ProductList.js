9df07396406aa69960fc4714aa8831b83fe20458
Serhii Lekhman <8.lsv.888@gmail.com>
2024-01-02 08:29:17 +0100

    fix btn

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -68,7 +68,7 @@ const ProductList = () => {
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
+                text: `Buy ${getTotalPrice(newItems)}`
             })
         }
     }
